0aa3b12b05924080f30d2863f8a557585ce19aaf
ali <a_icam@yahoo.com>
2020-07-08 16:55:02 +0430

    start processing data

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 input_dir = 'dataset/'
 import tensorflow as tf
+import pandas as pd
 from TrainUtils.model import *
 callbacks = [tf.keras.callbacks.ModelCheckpoint(filepath='best_model.h5',
                                             monitor='val_loss',
                                             save_best_only=True), ]
-model, feature_model = build_network()
-X_train, Y_train, X_test, Y_test = [], [], [], []
-history = model.fit(
-    X_train, Y_train,
-    epochs=100,
-    callbacks=callbacks,
-)
+# model, feature_model = build_network()
+# X_train, Y_train, X_test, Y_test = [], [], [], []
+# history = model.fit(
+#     X_train, Y_train,
+#     epochs=100,
+#     callbacks=callbacks,
+# )
+df = pd.read_hdf('store.h5')
